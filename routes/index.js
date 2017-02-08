d7c38e21193ccf5f1776f6257fa89098c7b759b6
Hasanga Somaratne <hasanga.charaka@gmail.com>
2017-02-08 12:34:54 +0530

    Add GPA calculation function

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,39 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db/db.json');
 
+const GPV = {
+  "A+" : 4,
+  "A" : 4,
+  "A-" : 3.75,
+  "B+" : 3.25,
+  "B" : 3,
+  "B-" : 2.75,
+  "C+": 2.25,
+  "C" : 2,
+  "C-" : 1.75,
+  "D+" : 1.25,
+  "D" : 1,
+  "D-" : 0.75,
+  "E" : 0
+}
+
+var calculateGPA = function(results){
+
+  var gpaResults = results.filter(function(res){
+    return res.grade != undefined && !res.code.startsWith("EN");
+  });
+
+  var totalCredits = gpaResults.reduce(function(a, b){
+    return a + parseInt(b.credits);
+  }, 0);
+
+  var totalGpv = gpaResults.reduce(function(a, b){
+    return a + (parseInt(b.credits) * GPV[b.grade]);
+  }, 0);
+
+  return totalGpv / totalCredits;
+}
+
 router.route('/get/:index')
       .get(function(req, res){
           var index = req.params.index;
@@ -16,6 +49,10 @@ router.route('/get/:index')
               }
           });
 
+          var gpa = calculateGPA(results);
+          console.log(gpa);
+
+          results.push({ gpa: gpa });
           res.json(results);
       });
 
