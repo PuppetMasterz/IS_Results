e8513450e61b7b9e46cda1651e1a3383839359dc
Malith Senaweera <malithsenaweera@gmail.com>
2017-02-08 23:15:36 +0000

    Change db stucture. Calculate semester-wise gpa

--- routes/index.js
+++ routes/index.js
@@ -20,42 +20,75 @@ const GPV = {
   "E" : 0
 }
 
-var calculateGPA = function(results){
+var getTotalCredits = function(results){
 
   var gpaResults = results.filter(function(res){
     return res.grade !== undefined && !res.code.startsWith("EN");
   });
 
-  var totalCredits = gpaResults.reduce(function(a, b){
+  return gpaResults.reduce(function(a, b){
     return a + parseInt(b.credits);
   }, 0);
+};
+
+var calculateGPA = function(results, totalCredits){
+
+  var gpaResults = results.filter(function(res){
+    return res.grade !== undefined && !res.code.startsWith("EN");
+  });
 
   var totalGpv = gpaResults.reduce(function(a, b){
     return a + (parseInt(b.credits) * GPV[b.grade]);
   }, 0);
 
   return totalGpv / totalCredits;
-}
+};
+
+var calculateCGPA = function(gpa, credits) {
+  var totalGpv = gpa.reduce(function(r,a,i){
+    return r + a * credits[i]
+  }, 0)
+
+  var sumCredits = credits.reduce(function(a, b) { return a + b; }, 0);
+
+  return totalGpv/sumCredits;
+};
+
 
 router.route('/get/:index')
-      .get(function(req, res){
-          var index = req.params.index;
+  .get(function(req, res){
+    var index = req.params.index;
+    var final = {};
+    var gpas = [];
+    var credits = [];
 
-          var results = db.data.map(function(subject){
-              return {
-                title: subject.Title,
-                code: subject.Subject_Code,
-                credits: subject.Credits,
-                semester: subject.Semester,
-                grade: subject.Marks[index]
-              }
-          });
+    for (year in db) {
+      final[year] = {};
+      for (semester in db[year]) {
+        final[year][semester] = {};
+        var results = db[year][semester].results.map(function(subject){
+          return {
+            title: subject.Title,
+            code: subject.Subject_Code,
+            credits: subject.Credits,
+            semester: subject.Semester,
+            grade: subject.Marks[index]
+          }
+        });
+        var totalCredits = getTotalCredits(results);
+        var gpa = calculateGPA(results, totalCredits);
+        gpas.push(gpa);
+        credits.push(totalCredits);
+        final[year][semester]['gpa'] = gpa
+        final[year][semester]['result'] = results;
+      }
 
-          var gpa = calculateGPA(results);
-          console.log(gpa);
 
-          results.push({ gpa: gpa });
-          res.json(results);
-      });
+    };
+
+    finalGpa = calculateCGPA(gpas, credits);
+    final['gpa'] = finalGpa;
+    res.json(final);
+  });
 
 module.exports = router;
